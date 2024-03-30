b6693969cf64448246c5e403c25f4f6b18e04283
Francisco Lino <franksuriel_7@hotmail.com>
2024-03-30 14:26:42 -0600

    UPDATE cart page

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,7 +1,73 @@
+import { Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
+import { QuantitySelector } from "../../../components/product/quantity-selector/QuantitySelector";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
 export default function CartPage() {
   return (
-    <div>
-      <h1>Cart Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px] ">
+        <Title title="Carrito" />
+        <div className="grid grid-cols-1 sm:grid-cols-2">
+          <div className="flex flex-col mt-5">
+            <span className="text-xl"> Agregar más items</span>
+            <Link href="/" className="underline mb-5">
+              Continua comprando
+            </Link>
+
+            {productsInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  width={100}
+                  height={100}
+                  alt={product.title}
+                  className="rounded mr-5"
+                />
+
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price}</p>
+                  <QuantitySelector quantity={3} />
+                  <button className="underline mt-3">Remover</button>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Resumen de orden</h2>
+            <div className="grid grid-cols-2">
+              <span>No. Productos</span>
+              <span className="text-right">3 artículos</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$300</span>
+
+              <span>Impuestos (15%)</span>
+              <span className="text-right">$45</span>
+
+              <span className="text-2xl mt-5">Total</span>
+              <span className="text-2xl mt-5 text-right">$345</span>
+            </div>
+            <div className="mb-5 mt-2 w-full">
+              <Link
+                className="flex btn-primary justify-center"
+                href="/checkout/address"
+              >
+                Checkout
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
