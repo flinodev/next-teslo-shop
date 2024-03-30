7fcd4bfb8c6506f615cfdab384efb13beb0049ca
Francisco Lino <franksuriel_7@hotmail.com>
2024-03-30 01:29:38 -0600

    ADD 404 not found page

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,7 +1,22 @@
-export default function CategoryPage() {
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default function CategoryPage({ params }: Props) {
+  const { id } = params;
+  const categories = ["kids", "men", "women"];
+
+  if (!categories.includes(id)) {
+    notFound();
+  }
+
   return (
     <div>
-      <h1>Category Page</h1>
+      <h1>Category Page {id}</h1>
     </div>
   );
 }
