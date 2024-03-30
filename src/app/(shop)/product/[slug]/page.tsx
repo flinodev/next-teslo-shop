5f93fa9780eea3ff229c3d50f3cd9de2049875c0
Francisco Lino <franksuriel_7@hotmail.com>
2024-03-30 14:26:21 -0600

    ADD products details

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,63 @@
-export default function ProductPage() {
+import {
+  ProductMobileSlideshow,
+  ProductSlideshow,
+  QuantitySelector,
+  SizeSelector,
+} from "@/components";
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export default function ProductPage({ params }: Props) {
+  const { slug } = params;
+
+  const product = initialData.products.find((p) => p.slug === slug);
+  if (!product) {
+    notFound();
+  }
+
   return (
-    <div>
-      <h1>Product by slug Page</h1>
+    <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+      {/* Slideshow */}
+      <div className="col-span-1 md:col-span-2">
+        <ProductMobileSlideshow
+          images={product.images}
+          title={product.title}
+          className="block md:hidden"
+        />
+        <ProductSlideshow
+          images={product.images}
+          title={product.title}
+          className="hidden md:block"
+        />
+      </div>
+      {/* Details */}
+      <div className="col-span-1 px-5">
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">${product.price}</p>
+        {/* Size Selector */}
+        <SizeSelector
+          selectedSize={product.sizes[0]}
+          availableSizes={product.sizes}
+        />
+        {/* Qty selector */}
+        <QuantitySelector quantity={2} />
+
+        {/* Add to cart button */}
+        <button className="btn-primary my-5">Agregar al carrito</button>
+
+        {/* Description */}
+        <h3 className="font-bold text-sm">Descripción</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
     </div>
   );
 }
