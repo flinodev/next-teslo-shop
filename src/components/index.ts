9a2909aadc14375528d79003d5233ed81530ae46
Francisco Lino <franksuriel_7@hotmail.com>
2024-03-30 02:14:30 -0600

    ADD product item and product grid

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
+export * from "./products/product-grid/ProductGrid";
+export * from "./products/product-grid/ProductGridItem";
+
 export * from "./ui/not-found/NotFoundPage";
 export * from "./ui/title/Title";
 export * from "./ui/top-menu/TopMenu";
