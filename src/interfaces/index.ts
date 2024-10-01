9640bde182bafe1abbe33d2d6adebcdd47189f52
Francisco Lino <franksuriel_7@hotmail.com>
2024-10-01 09:43:01 -0600

    ADD User interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,4 @@ export * from "./address.interface";
 export * from "./country.interface";
 export * from "./payPalOrderStatusResponse.interface";
 export * from "./product.interface";
+export * from "./user.interface";
